de64060110c00352bc65bd58054651049d13cf4b
Strawberries <crowles98@gmail.com>
2023-10-07 09:54:13 +0100

    Update dataset.py

--- animatediff/data/dataset.py
+++ animatediff/data/dataset.py
@@ -3,12 +3,13 @@ import numpy as np
 from einops import rearrange
 
 import torch
-#from decord import VideoReader
+from decord import VideoReader
 import cv2
 
 import torchvision.transforms as transforms
 from torch.utils.data.dataset import Dataset
 from animatediff.utils.util import zero_rank_print
+from torchvision.io import read_image
 
 
 
@@ -23,120 +24,124 @@ class WebVid10M(Dataset):
         with open(csv_path, 'r') as csvfile:
             self.dataset = list(csv.DictReader(csvfile))
         self.length = len(self.dataset)
-        zero_rank_print(f"data scale: {self.length}")
+        print(f"data scale: {self.length}")
             
         self.video_folder    = video_folder
         self.sample_stride   = sample_stride
         self.sample_n_frames = sample_n_frames
         self.is_image        = is_image
-        
+        print("length",len(self.dataset))
         sample_size = tuple(sample_size) if not isinstance(sample_size, int) else (sample_size, sample_size)
+        print("sample size",sample_size)
         self.pixel_transforms = transforms.Compose([
             transforms.RandomHorizontalFlip(),
-            transforms.Resize(sample_size[0]),
             transforms.CenterCrop(sample_size),
+            transforms.Resize(sample_size),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
         ])
     
 
     def get_batch_old(self, idx):
-        video_dict = self.dataset[idx]
-        videoid, name, page_dir = video_dict['videoid'], video_dict['name'], video_dict['page_dir']
-        
-        video_dir = os.path.join(self.video_folder, f"{videoid}.mp4")
-        
-
-        video_capture = cv2.VideoCapture(video_dir)
-        
-        if not video_capture.isOpened():
-            raise ValueError(f"Could not open video file: {video_dir}")
-        
-        video_length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-        
-        if not self.is_image:
-            clip_length = min(video_length, (self.sample_n_frames - 1) * self.sample_stride + 1)
-            start_idx = 0
-            batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
-        else:
-            batch_index = [random.randint(0, video_length - 1)]
-        
-        frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        pixel_values = np.empty((len(batch_index), frame_height, frame_width, 3), dtype=np.uint8)
-        
-        for i, idx in enumerate(batch_index):
-            video_capture.set(cv2.CAP_PROP_POS_FRAMES, idx)
-            ret, frame = video_capture.read()
-            if not ret:
-                video_capture.release()
-                raise ValueError(f"Could not read frame {idx} from video file: {video_dir}")
-            # Convert the frame from BGR to RGB
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            pixel_values[i] = frame_rgb
-
-        video_capture.release()
-        pixel_values = torch.tensor(pixel_values).permute(0, 3, 1, 2).contiguous() * (1 / 255.0)
-
-        if self.is_image:
-            pixel_values = pixel_values[0]
-        pixel_values = pixel_values * 2 - 1
-        return pixel_values, name
-
-
-
-
-    def get_batch(self, idx):
         while True:
-            try:
-                video_dict = self.dataset[idx]
-                videoid, name, page_dir = video_dict['videoid'], video_dict['name'], video_dict['page_dir']
+            video_dict = self.dataset[idx]
+            videoid, name, page_dir = video_dict['videoid'], video_dict['name'], video_dict['page_dir']
         
-                video_dir = os.path.join(self.video_folder, page_dir, f"{videoid}.mp4")
-                # Check if the file exists
-                if not os.path.exists(video_dir):
-                    #print(f"File not found: {video_dir}")
-                    idx = random.randint(0, len(self.dataset) - 1)
-                    continue  # try the next index
+            video_dir = os.path.join(self.video_folder, f"{videoid}.mp4")
+            # If the video doesn't exist, return pixel values filled with zeros
+            if not os.path.exists(video_dir):
+                
+                idx = idx+ 1
+                continue
+               
+                
+            video_reader = VideoReader(video_dir)
+            video_length = len(video_reader)
+        
+            if not self.is_image:
+                clip_length = min(video_length, (self.sample_n_frames - 1) * self.sample_stride + 1)
+                start_idx = random.randint(0, video_length - clip_length)
+                batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
+            else:
+                batch_index = [random.randint(0, video_length - 1)]
+        
+            pixel_values = torch.from_numpy(video_reader.get_batch(batch_index).asnumpy()).permute(0, 3, 1, 2).contiguous()
+            pixel_values = pixel_values / 255.
+            del video_reader
+        
+            if self.is_image:
+                pixel_values = pixel_values[0]
+        
+            return pixel_values, name
+
+
+
+    def get_batch_webvid_video(self, idx):
+        while True:
+            video_dict = self.dataset[idx]
+            videoid, name, page_dir = video_dict['videoid'], video_dict['name'], video_dict['page_dir']
+            
+            video_dir = os.path.join(self.video_folder, page_dir, f"{videoid}.mp4")
+            
+            # Check if the file exists
+            if not os.path.exists(video_dir):
+                #print(f"File not found: {video_dir}")
+                idx = random.randint(0, len(self.dataset) - 1)
+                continue  # try the next index
+            
+            video_reader = VideoReader(video_dir)
+            video_length = len(video_reader)
+            
+            if not self.is_image:
+                clip_length = min(video_length, (self.sample_n_frames - 1) * self.sample_stride + 1)
+                start_idx = random.randint(0, video_length - clip_length)
+                batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
+            else:
+                batch_index = [random.randint(0, video_length - 1)]
+            
+            pixel_values = torch.from_numpy(video_reader.get_batch(batch_index).asnumpy()).permute(0, 3, 1, 2).contiguous()
+            pixel_values = pixel_values / 255.
+            del video_reader
+            
+            if self.is_image:
+                pixel_values = pixel_values[0]
+            
+            return pixel_values, name
+            
+    def get_batch(self, idx):
+        def sort_frames(frame_name):
+            # Extract the frame number and convert it to an integer
+            return int(frame_name.split('_')[1].split('.')[0])
     
-                video_capture = cv2.VideoCapture(video_dir)
-        
-                if not video_capture.isOpened():
-                    raise ValueError(f"Could not open video file: {video_dir}")
-        
-                video_length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
-        
-                if not self.is_image:
-                    clip_length = min(video_length, (self.sample_n_frames - 1) * self.sample_stride + 1)
-                    start_idx = 0
-                    batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
-                else:
-                    batch_index = [random.randint(0, video_length - 1)]
-        
-                frame_height = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                frame_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-                pixel_values = np.empty((len(batch_index), frame_height, frame_width, 3), dtype=np.uint8)
-        
-                for i, idx in enumerate(batch_index):
-                    video_capture.set(cv2.CAP_PROP_POS_FRAMES, idx)
-                    ret, frame = video_capture.read()
-                    if not ret:
-                        video_capture.release()
-                        raise ValueError(f"Could not read frame {idx} from video file: {video_dir}")
-                    # Convert the frame from BGR to RGB
-                    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    pixel_values[i] = frame_rgb
-        
-                video_capture.release()
-                pixel_values = torch.tensor(pixel_values).permute(0, 3, 1, 2).contiguous() * (1 / 255.0)
-        
-                if self.is_image:
-                    pixel_values = pixel_values[0]
-        
-                return pixel_values, name
-            except Exception as inst:
-                print(type(inst))    # the exception type
-                print(inst.args)     # arguments stored in .args
-                print(inst)   
+        while True:
+            video_dict = self.dataset[idx]
+            videoid, name, page_dir = video_dict['videoid'], video_dict['name'], video_dict['page_dir']
+    
+            # Directory where preprocessed images for the video are stored
+            preprocessed_dir = os.path.join(self.video_folder, videoid)
+    
+            # Check if the directory exists
+            if not os.path.exists(preprocessed_dir):
+                idx = random.randint(0, len(self.dataset) - 1)
+                continue  # try the next index
+    
+            if not self.is_image:
+                image_files = sorted(os.listdir(preprocessed_dir), key=sort_frames)
+                total_frames = len(image_files)
+                clip_length = min(total_frames, (self.sample_n_frames - 1) * self.sample_stride + 1)
+                start_idx = random.randint(0, total_frames - clip_length)
+                batch_index = np.linspace(start_idx, start_idx + clip_length - 1, self.sample_n_frames, dtype=int)
+            else:
+                image_files = random.choice(os.listdir(preprocessed_dir))
+                batch_index = [image_files]
+    
+            # Read images from the directory
+            pixel_values = torch.stack([read_image(os.path.join(preprocessed_dir, image_files[int(i)])) for i in batch_index])
+            pixel_values = pixel_values.float() / 255.  # Convert from uint8 to float and normalize
+    
+            if self.is_image:
+                pixel_values = pixel_values[0]
+    
+            return pixel_values, name
 
     
     def __len__(self):
@@ -149,6 +154,7 @@ class WebVid10M(Dataset):
                 break
 
             except Exception as e:
+                print (e)
                 idx = random.randint(0, self.length-1)
 
         pixel_values = self.pixel_transforms(pixel_values)
